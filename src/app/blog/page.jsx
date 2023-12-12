3eda8c76e2ea6ce1bec112f7aa1a30c000300d7a
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2023-12-12 18:01:25 +0530

    Day06

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import styles from './page.module.css'
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
-  )
-}
+    <div className={styles.container}>
+      <Link className={styles.container} href="/testId">
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/19419507/pexels-photo-19419507/free-photo-of-a-laptop-with-a-clock-on-it-sitting-on-a-couch.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.desc}
+          />
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import styles from './page.module.css'
-const Category = () => {
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
+const Category = ({ params }) => {
+  // usage of params for change items
+  console.log(params);
   return (
-    <div className={styles.container}>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/4099235/pexels-photo-4099235.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
